fe47a7ca5e740a38caf5f9d2271bb10dcd05cef8
paul-wade <paul.wade@live.com>
2014-06-22 12:22:29 -0500

    sanitize post body to format html in blog post

--- public/app/post/mbPostsCtrl.js
+++ public/app/post/mbPostsCtrl.js
@@ -2,10 +2,17 @@
 
 /* Controllers */
 
-angular.module('mbApp').controller('mbPostsCtrl',  function($scope,PostService) {
+angular.module('mbApp').controller('mbPostsCtrl', function ($scope, $sce, PostService) {
 
     PostService.query(function(response){
+
+        //sanitize post body to display html on page.
+        for (var i=0; i<response.length;i++) {
+            response[i].body = $sce.trustAsHtml(response[i].body);
+        }
+
         $scope.posts = response;
+
     });
 
 });
